493d257bff8d3eeec8d9a5746d917a4f468a5d31
kuranosuke88 <takorou88@yahoo.co.jp>
2019-12-18 11:59:58 +0000

    完成

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,5 @@
 class AttendancesController < ApplicationController
+include AttendancesHelper
   before_action :set_user, only: %i(edit_one_month update_one_month edit_one_week update_one_week)
   before_action :logged_in_user, only: %i(update edit_one_month edit_one_week)
   before_action :admin_or_correct_user, only: %i(update, edit_one_month update_one_month edit_one_week update_one_week)
@@ -32,16 +33,21 @@ class AttendancesController < ApplicationController
   
   def update_one_month
     ActiveRecord::Base.transaction do
-      attendances_params.each do |id, item|
-        attendance = Attendance.find(id)
-        attendance.update_attributes!(item)
+      if attendances_invalid?
+        attendances_params.each do |id, item|
+          attendance = Attendance.find(id)
+          attendance.update_attributes!(item)
       end
+        flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+        redirect_to user_url(date: params[:date])
+      else
+        flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+        redirect_to attendances_edit_one_month_user_url(date: params[:date])
+      end
+    rescue ActiveRecord::RecordInvalid
+      flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+      redirect_to attendances_edit_one_month_user_url(date: params[:date])
     end
-    flash[:success] = "１か月分の勤怠情報を更新しました。"
-    redirect_to user_url(date: params[:date])
-  rescue ActiveRecord::RecordInvalid
-    flash[:danger] = "無効な入力データがあったため、更新をキャンセルしました。"
-    redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
   
   def edit_one_week
@@ -49,16 +55,21 @@ class AttendancesController < ApplicationController
   
   def update_one_week
      ActiveRecord::Base.transaction do
-      attendances_params.each do |id, item|
-        attendance = Attendance.find(id)
-        attendance.update_attributes!(item)
+      if attendances_invalid?
+        attendances_params.each do |id, item|
+          attendance = Attendance.find(id)
+          attendance.update_attributes!(item)
       end
+        flash[:success] = "1週間分の勤怠情報を更新しました。"
+        redirect_to user_url(date: params[:date])
+      else
+        flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+        redirect_to attendances_edit_one_week_user_url(date: params[:date])
+      end
+    rescue ActiveRecord::RecordInvalid
+      flash[:danger] = "無効な入力データがあったため、更新をキャンセルしました。"
+      redirect_to attendances_edit_one_week_user_url(date: params[:date])
     end
-    flash[:success] = "1週間分の勤怠情報を更新しました。"
-    redirect_to user_url(date: params[:date])
-  rescue ActiveRecord::RecordInvalid
-    flash[:danger] = "無効な入力データがあったため、更新をキャンセルしました。"
-    redirect_to attendances_edit_one_week_user_url(date: params[:date])
   end
   
   private
@@ -74,5 +85,4 @@ class AttendancesController < ApplicationController
         redirect_to(root_url)
       end
     end
-  
 end
